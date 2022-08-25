e5c8456fff5340ad8ca749a978f06d468c96f10e
Vipulvatsa07 <vatsavipul.8@gmail.com>
2022-08-25 22:32:40 +0530

    day3-progress

---
+++ scripts/bloginside.js
@@ -0,0 +1,10 @@
+document.querySelector("#tweet").addEventListener("click",myfunction)
+function myfunction()
+{
+    window.location="https://twitter.com/ClientEarth/status/1556571993948012544?ref_src=twsrc%5Etfw%7Ctwcamp%5Etweetembed%7Ctwterm%5E1556571993948012544%7Ctwgr%5Efbb220d3d7c680e7fdf9da7894f2593879c3cba1%7Ctwcon%5Es1_&ref_url=https%3A%2F%2Fbuffer.com%2Fresources%2Fnational-nonprofit-day%2F"
+}
+document.querySelector("#tweet2").addEventListener("click",myfunction)
+function myfunction()
+{
+    window.location="https://twitter.com/LiftingLA/status/1540386753298796544?ref_src=twsrc%5Etfw%7Ctwcamp%5Etweetembed%7Ctwterm%5E1540386753298796544%7Ctwgr%5Efbb220d3d7c680e7fdf9da7894f2593879c3cba1%7Ctwcon%5Es1_&ref_url=https%3A%2F%2Fbuffer.com%2Fresources%2Fnational-nonprofit-day%2F"
+}
