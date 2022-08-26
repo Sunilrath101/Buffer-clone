1a0db8c47f38c973290ced2d1ce18dba1b4fa9fa
Vipulvatsa07 <vatsavipul.8@gmail.com>
2022-08-26 23:35:17 +0530

    day4 progress

--- components/navbar_blog.js
+++ components/navbar_blog.js
@@ -2,19 +2,19 @@ function navbar(){
     return ` <div id="navbar">
     <div>
         <img src="https://buffer.com/resources/content/images/2021/02/buffer-logo.svg" alt="">
-        <h5>| BLOG</h5>
+        <h5>| <a href="./blog_homepage.html">Blog</a></h5>
     </div>
     <div id="second_div">
-        <p><a href="">Guides</a></p>
-        <p><a href="">Trends</a></p>
-        <p><a href="">News</a></p>
-        <p><a href="">Work Culture</a></p>
+        <p><a href="./guides.html">Guides</a></p>
+        <p><a href="./trends.html">Trends</a></p>
+        <p><a href="./news.html">News</a></p>
+        <p><a href="./workculture.html">Work Culture</a></p>
         <p><a href="">All categories</a></p>
 
     </div>
     <div id="third_div">
         <p><i class="fa-solid fa-magnifying-glass"></i></p>
-        <p><a href="">Get started for free</a></p>
+        <p><a href="./home.html">Get started for free</a></p>
     </div>
 </div>`
 };
