d446abe26d3217a7c27693c1ea9cd527899388f2
vinaykumar7580 <vinayhatwar16@gmail.com>
2022-08-26 15:03:55 +0530

    file added

---
+++ components/footer_blog.js
@@ -0,0 +1,35 @@
+
+function footer(){
+    return ` <div id="footer">
+    <div>
+        <img src="https://buffer.com/resources/content/images/2021/02/buffer-logo.svg" alt="">
+    </div>
+    <div>
+        <h5>Category</h5>
+        <p>Social Media Marketing</p>
+        <p>Instagram Marketing</p>
+        <p>Social Analytics</p>
+        <p>News and Trends</p>
+        <p>Best apps and tools</p>
+        <p>Inside Buffer</p>
+        <p>Remote Work</p>
+        <p>Transparency</p>
+    </div>
+    <div>
+        <h5>Blog & podcasts</h5>
+        <p>Science of Social Media</p>
+        <p>Breaking Brand</p>
+        <p>Social Media Blog</p>
+        <p>Open Blog</p>
+    </div>
+    <div>
+        <h5>About Buffer</h5>
+        <p>Social media solutions</p>
+        <p>Our culture</p>
+        <p>Our value</p>
+    </div>
+
+</div>
+`
+}
+export default footer
