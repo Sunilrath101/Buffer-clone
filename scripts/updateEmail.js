e9ba82651e1c9f703eb4ae68e37182f68c1701ad
Sunilrath101 <sunilrath.ssr@gmail.com>
2022-08-28 14:35:41 +0530

    bug fix

--- scripts/updateEmail.js
+++ scripts/updateEmail.js
@@ -1,6 +1,16 @@
 let users = JSON.parse(localStorage.getItem("bufferEmail"));
 console.log("users:", users);
-let emailuser = document.querySelector("#useremail");
-emailuser.innerText = users;
-emailuser.style.fontSize = "8px";
-emailuser.style.color = "gray";
+
+// function updateee(users) {
+
+// }
+// updateee(users);
+
+setTimeout(() => {
+  let emailuser = document.querySelector("#useremail");
+  emailuser.innerText = users;
+  emailuser.style.fontSize = "8px";
+  emailuser.style.color = "gray";
+
+  // emailuser.append(pera);
+}, 500);
